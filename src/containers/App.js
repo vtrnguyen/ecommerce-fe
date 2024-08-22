89edd4255d27299089e52661b3d752be534772ac
Vo Trung Nguyen <votrungnguyenlop2a@gmail.com>
2024-08-23 05:09:31 +0700

    create nav-header, routing

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component, Fragment } from "react";
 import './App.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import Login from "./Login/Login";
+import Login from "./Login/Login";
 import HomePage from "./HomePage/HomePage";
 
 class App extends Component {
@@ -9,7 +10,13 @@ class App extends Component {
   render() {
     return (
       <Fragment>
-        <HomePage />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </BrowserRouter>
       </Fragment>
     )
   }
--- src/containers/HomePage/HomeHeader.js
+++ src/containers/HomePage/HomeHeader.js
@@ -6,52 +6,91 @@ class HomeHeader extends Component {
         super(props);
         this.state = {
         }
-    }    
+    }
 
     render() {
         return (
             <>
-                <div className="hheader-container">
-                    <div className="logo"><i className="fas fa-shopping-cart"></i> Origin Dev</div>
-                    <div className="header-content">
-                        <div className="basic-nav-bar">
-                            <div className="nav-item homepage">
-                                <i class="fas fa-home"></i>
-                                Trang chủ
-                            </div>
-                            <div className="nav-item contact">
-                                <i class="fab fa-instagram"></i>
-                                <i class="fab fa-facebook"></i>
-                                Liên hệ
-                            </div>
-                            <div className="nav-item support">
-                                <i class="fas fa-question-circle"></i>
-                                Hỗ trợ
-                            </div>
-                            <div className="nav-item choose-language">
-                                <i class="fas fa-globe"></i>
-                                Tiếng Việt
-                                <i class="fas fa-chevron-down"></i>
-                            </div>
-                            <div className="nav-item my-account">
-                                <i class="fas fa-user"></i>
-                                Tài khoản của tôi
-                            </div>
-                        </div>
-                        <div className="display-infor">
-                            <div className="search-bar">
-                                <input
-                                    className="search-input"
-                                    placeholder="Bạn muốn mua gì hôm nay?" 
-                                />
-                                <div className="search-icon">
-                                    <i className="fas fa-search"></i>
+                <div className="hheader-wrapper">
+                    <div className="main-header">
+                        <div className="logo"><i className="fas fa-shopping-cart"></i> Origin Dev</div>
+                        <div className="header-content">
+                            <div className="basic-nav-bar">
+                                <div className="nav-item homepage">
+                                    <i class="fas fa-home"></i>
+                                    Trang chủ
+                                </div>
+                                <div className="nav-item contact">
+                                    <i class="fab fa-instagram"></i>
+                                    <i class="fab fa-facebook"></i>
+                                    Liên hệ
+                                </div>
+                                <div className="nav-item support">
+                                    <i class="fas fa-question-circle"></i>
+                                    Hỗ trợ
+                                </div>
+                                <div className="nav-item choose-language">
+                                    <i class="fas fa-globe"></i>
+                                    Tiếng Việt
+                                    <i class="fas fa-chevron-down"></i>
+                                </div>
+                                <div className="nav-item my-account">
+                                    <i class="fas fa-user"></i>
+                                    Tài khoản của tôi
                                 </div>
                             </div>
-                            <div className="infor-icon">
-                                <i className="fas fa-bell"></i>
-                                <i className="fas fa-cart-arrow-down"></i>
+                            <div className="display-infor">
+                                <div className="search-container">
+                                    <div className="search-bar">
+                                        <input
+                                            className="search-input"
+                                            placeholder="Bạn muốn mua gì hôm nay?" 
+                                        />
+                                        <div className="search-icon">
+                                            <i className="fas fa-search"></i>
+                                        </div>
+                                    </div>
+                                    <div className="search-recommened">
+                                        <a href="facebook.com">đồng hồ trẻ em</a>
+                                        <a href="facebook.com">chuột Attact Shark R1</a>
+                                        <a href="facebook.com">tăm chỉ nha khoa</a>
+                                        <a href="facebook.com">áo Barcelona</a>
+                                        <a href="facebook.com">iPhone 15 Pro Max</a>
+                                        <a href="facebook.com">xe máy Vision 2024</a>
+                                    </div>
+                                </div>
+                                <div className="infor-icon">
+                                    <i className="fas fa-bell"></i>
+                                    <i className="fas fa-cart-arrow-down"></i>
+                                </div>
                             </div>
+                        </div>
+                    </div>
+                    <div className="nav-header">
+                        <div className="promise-title">Cam kết</div>
+                        <div className="promise-item authentic-product">
+                            <i class="fas fa-check"></i>
+                            100% hàng thật
+                        </div>
+                        <div className="promise-item fake-product">
+                            <i class="fas fa-undo-alt"></i>
+                            Hoàn 200% nếu hàng giả
+                        </div>
+                        <div className="promise-item return-product">
+                            <i class="fas fa-exchange-alt"></i>
+                            30 ngày đổi trả
+                        </div>
+                        <div className="promise-item fast-delivery">
+                            <i class="fas fa-truck"></i>
+                            Giao hàng nhanh 2h
+                        </div>
+                        <div className="promise-item cheap-price">
+                            <i class="fas fa-tag"></i>
+                            Giá siêu rẻ
+                        </div>
+                        <div className="promise-item free-shiping">
+                            <i class="fas fa-wallet"></i>
+                            Freeship mọi đơn
                         </div>
                     </div>
                 </div>
